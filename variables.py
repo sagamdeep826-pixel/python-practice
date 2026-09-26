6ed7f8147bbf403e0f45e690e0c5fb156d0d46b7
indentation-error <sagamdeep826@gmail.com>
2026-09-26 13:42:56 +0530

    Delete variables.py

--- variables.py
+++
@@ -1,6 +0,0 @@
-name = "bob the builder"
-age = 30
-address = "123 Construction Lane"
-height = 5.9
-building = True
-print("hello my name is", name, "and I am", age, "years old. I live at", address, "and I am", height, "feet tall. It is", building, "that I build buildings.")
